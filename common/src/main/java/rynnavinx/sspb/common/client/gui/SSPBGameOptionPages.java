08cc41691507f42f96a01f7a1c23409bc1e64af4
Lonmo0208 <2675932757@qq.com>
2025-03-14 16:50:52 +0800

    Up

--- common/src/main/java/rynnavinx/sspb/common/client/gui/SSPBGameOptionPages.java
+++ common/src/main/java/rynnavinx/sspb/common/client/gui/SSPBGameOptionPages.java
@@ -1,7 +1,6 @@
 package rynnavinx.sspb.common.client.gui;
 
 import com.google.common.collect.ImmutableList;
-
 import me.jellysquid.mods.sodium.client.gui.options.OptionFlag;
 import me.jellysquid.mods.sodium.client.gui.options.OptionGroup;
 import me.jellysquid.mods.sodium.client.gui.options.OptionImpl;
@@ -9,19 +8,14 @@ import me.jellysquid.mods.sodium.client.gui.options.OptionPage;
 import me.jellysquid.mods.sodium.client.gui.options.control.ControlValueFormatter;
 import me.jellysquid.mods.sodium.client.gui.options.control.SliderControl;
 import me.jellysquid.mods.sodium.client.gui.options.control.TickBoxControl;
-
 import net.minecraft.network.chat.Component;
-
 import rynnavinx.sspb.common.client.gui.options.storage.SSPBOptionsStorage;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class SSPBGameOptionPages {
-
     private static final SSPBOptionsStorage sspbOpts = new SSPBOptionsStorage();
-
 
     public static OptionPage sspb() {
         List<OptionGroup> groups = new ArrayList<>();
@@ -30,7 +24,7 @@ public class SSPBGameOptionPages {
                         .setName(Component.translatable("sspb.options.shadowyness.name"))
                         .setTooltip(Component.translatable("sspb.options.shadowyness.tooltip"))
                         .setControl(option -> new SliderControl(option, 0, 100, 1, ControlValueFormatter.percentage()))
-                        .setBinding(SSPBGameOptions::updateShadowyness, opts -> opts.shadowynessPercent)
+                        .setBinding((opts, value) -> opts.shadowynessPercent = value, opts -> opts.shadowynessPercent)
                         .setFlags(OptionFlag.REQUIRES_RENDERER_RELOAD)
                         .build())
                 .add(OptionImpl.createBuilder(boolean.class, sspbOpts)
--- forge/src/main/java/rynnavinx/sspb/forge/client/SSPBForgeClientMod.java
+++ forge/src/main/java/rynnavinx/sspb/forge/client/SSPBForgeClientMod.java
@@ -1,5 +1,6 @@
 package rynnavinx.sspb.forge.client;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 
 import rynnavinx.sspb.common.client.SSPBClientMod;
@@ -8,7 +9,7 @@ import rynnavinx.sspb.common.client.SSPBClientMod;
 @Mod("sspb")
 public class SSPBForgeClientMod {
 
-	public SSPBForgeClientMod() {
+	public SSPBForgeClientMod(IEventBus eventBus) {
 		SSPBClientMod.onInitClient();
 	}
 }
